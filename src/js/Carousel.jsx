bd482c6396d04fee20411bb31b6951b0ea78a6d0
이준우 <junwoo.lee@ridi.com>
2018-01-15 14:01:37 +0900

    update.

--- src/js/Carousel.jsx
+++ src/js/Carousel.jsx
@@ -6,91 +6,97 @@ export const CarouselNavigatorType = {
   NUMBER: '1'
 };
 
+export const DOMEventConstants = {
+  CLICK: 'click',
+  SCROLL: 'scroll',
+  TOUCH_MOVE: 'touchmove',
+  MOUSE_WHEEL: 'mousewheel',
+  KEY_DOWN: 'keydown',
+  RESIZE: 'resize',
+  FOCUS: 'focus',
+  BLUR: 'blur',
+  ERROR: 'error',
+  CHANGE: 'change',
+  ANIMATION_END: 'animationend',
+  TRANSITION_END: 'transitionend',
+  WEBKIT_TRANSITION_END: 'webkitTransitionEnd',
+};
+
 class Carousel extends React.Component {
   constructor() {
     super();
     this.slidingArea = null;
     this.timer = null;
-    this.setAfterTransitionFunc = null;
-    this.setAfterWindowResizeFunc = null;
-    this.windowFocusOnFunc = null;
-    this.windowFocusOutFunc = null;
-    this.autoPlayer = null;
-    this.clickSafe = false;
+    this.maximumDrag = 200;
+    this.clickSafe = true;
     this.touchObject = {};
+    this.draggable = true;
+    this.sliderItemWidth = 100;
     this.state = {
-      dragging: false,
       currentSlide: 1,
       tempPositionX: undefined,
       currentPositionX: 0,
       slideCount: 0,
       autoPlaying: false
     };
+
+    this._setAfterWindowResize = this._setAfterWindowResize.bind(this);
+    this._setAfterTransition = this._setAfterTransition.bind(this);
+    this._windowFocusOn = this._windowFocusOn.bind(this);
+    this._windowFocusOut = this._windowFocusOut.bind(this);
+    this._autoPlayer = this._autoPlayer.bind(this);
   }
 
   componentWillMount() {
-    const state = {};
-    state.slideCount = this.props.children.length;
-    if (this.props.targetSlide > 1) {
-      state.currentSlide = this.props.targetSlide;
-      state.currentPositionX = this.getHorizontalPosition(this.props.targetSlide);
-    } else {
-      state.currentSlide = 1;
-      state.currentPositionX = this.getHorizontalPosition(1);
-    }
-    this.setState(state);
+    this.sliderItemWidth = this.props.isFullWidth ? window.innerWidth : this.props.elementWidth;
+    this._setCurrentSlideAndPosition(this.props);
   }
 
   componentDidMount() {
-    this.setAfterWindowResizeFunc = this._setAfterWindowResize.bind(this);
-    this.setAfterTransitionFunc = this._setAfterTransition.bind(this);
-    this.windowFocusOnFunc = this._windowFocusOn.bind(this);
-    this.windowFocusOutFunc = this._windowFocusOut.bind(this);
-    this.autoPlayer = this._autoPlayer.bind(this);
-
-    window.addEventListener('resize', this.setAfterWindowResizeFunc);
-    window.addEventListener('focus', this.windowFocusOnFunc);
-    window.addEventListener('blur', this.windowFocusOutFunc);
+    window.addEventListener(DOMEventConstants.RESIZE, this._setAfterWindowResize);
+    window.addEventListener(DOMEventConstants.FOCUS, this._windowFocusOn);
+    window.addEventListener(DOMEventConstants.BLUR, this._windowFocusOut);
     this.setTimerStart();
   }
 
   componentWillReceiveProps(nextProps) {
-    this.setState({
-      slideCount: nextProps.children.length,
-      currentSlide: nextProps.targetSlide,
-      currentPositionX: this.getHorizontalPosition(nextProps.targetSlide)
-    });
+    this._setCurrentSlideAndPosition(nextProps);
   }
 
   componentWillUnmount() {
-    if (this.setAfterWindowResizeFunc) {
-      window.removeEventListener('resize', this.setAfterWindowResizeFunc);
+    const { saveStateFunc, isResetToFirst } = this.props;
+    const { currentSlide } = this.state;
+    if (this._setAfterWindowResize) {
+      window.removeEventListener(DOMEventConstants.RESIZE, this._setAfterWindowResize);
     }
-    if (this.windowFocusOnFunc) {
-      window.removeEventListener('focus', this.windowFocusOnFunc);
+    if (this._windowFocusOn) {
+      window.removeEventListener(DOMEventConstants.FOCUS, this._windowFocusOn);
     }
-    if (this.windowFocusOutFunc) {
-      window.removeEventListener('blur', this.windowFocusOutFunc);
+    if (this._windowFocusOut) {
+      window.removeEventListener(DOMEventConstants.BLUR, this._windowFocusOut);
     }
-    if (this.setAfterTransitionFunc) {
-      this.slidingArea.removeEventListener('transitionend', this.setAfterTransitionFunc);
+    if (this._setAfterTransition) {
+      this.setAfterTransitionEventListener(true);
     }
     if (this.timer) {
       clearInterval(this.timer);
     }
-  }
 
+    if (saveStateFunc) {
+      if (isResetToFirst) {
+        saveStateFunc(1);
+      } else {
+        saveStateFunc(currentSlide);
+      }
+    }
+  }
 
   // TouchEvent ----------------------------------------------------------------------------------------------
   onTouchStart(e) {
-    if (this.state.dragging) {
-      return;
-    }
     this.touchObject = {
       startX: e.touches[0].pageX,
       startY: e.touches[0].pageY
     };
-    this.setState({ dragging: true });
     this.handleFocusOn();
   }
 
@@ -105,8 +111,9 @@ class Carousel extends React.Component {
     if (direction !== 0 || this.touchObject.length > 44) {
       e.preventDefault();
     }
-
-    const length = Math.round(Math.sqrt((e.touches[0].pageX - this.touchObject.startX) ** 2));
+    // ** 연산자는 빌드가 안되고 Math.pow 는 lint에 걸린다...
+    const movedLength = (e.touches[0].pageX - this.touchObject.startX);
+    const length = Math.round(Math.sqrt(movedLength * movedLength));
     let slidingLength = length;
 
     this.touchObject = {
@@ -118,8 +125,8 @@ class Carousel extends React.Component {
       direction
     };
 
-    if (length > 200) {
-      slidingLength = 200;
+    if (length > this.maximumDrag) {
+      slidingLength = this.maximumDrag;
     }
     this.setSlidingAreaLeft(-(slidingLength * direction));
   }
@@ -147,14 +154,10 @@ class Carousel extends React.Component {
   }
 
   onMouseDown(e) {
-    if (this.state.dragging) {
-      return;
-    }
     this.touchObject = {
       startX: e.clientX,
       startY: e.clientY
     };
-    this.setState({ dragging: true });
   }
 
   onMouseMove(e) {
@@ -168,8 +171,9 @@ class Carousel extends React.Component {
     if (direction !== 0 || this.touchObject.length > 44) {
       e.preventDefault();
     }
-
-    const length = Math.round(Math.sqrt((e.clientX - this.touchObject.startX) ** 2));
+    // ** 연산자는 빌드가 안되고 Math.pow 는 lint에 걸린다...
+    const movedLength = (e.clientX - this.touchObject.startX);
+    const length = Math.round(Math.sqrt(movedLength * movedLength));
 
     this.touchObject = {
       startX: this.touchObject.startX,
@@ -200,7 +204,7 @@ class Carousel extends React.Component {
     const { autoPlaying } = this.state;
     if (isAutoPlay && !autoPlaying) {
       this.timer = setInterval(() => {
-        this.autoPlayer();
+        this._autoPlayer();
       }, Number(autoPlayInterval));
       this.setState({ autoPlaying: true });
     }
@@ -217,20 +221,18 @@ class Carousel extends React.Component {
 
   getHorizontalPosition(targetSlider) {
     const {
-      elementWidth,
       isInfinite,
       isFullWidth,
       containerWidth
     } = this.props;
-
     let wrapperWidth = containerWidth;
     if (isFullWidth) {
       wrapperWidth = window.innerWidth;
     }
 
-    let positionX = (wrapperWidth / 2) - ((Number(elementWidth) * (Number(targetSlider) - 1)) + (Number(elementWidth) / 2));
+    let positionX = (wrapperWidth / 2) - ((Number(this.sliderItemWidth) * (Number(targetSlider) - 1)) + (Number(this.sliderItemWidth) / 2));
     if (isInfinite) {
-      positionX -= (Number(elementWidth) * 2);
+      positionX -= (Number(this.sliderItemWidth) * 2);
     }
 
     this.setState({ currentPositionX: positionX });
@@ -251,15 +253,45 @@ class Carousel extends React.Component {
     if (this.slidingArea === null || this.slidingArea === undefined) {
       return;
     }
-    this.slidingArea.style.transition = `transform ${duration}ms ${cssEase}`;
+    this.setTransitionPrefixed(`transform,-webkit-transform ${duration}ms ${cssEase}`);
 
     if (isInfinite) {
-      this.slidingArea.removeEventListener('transitionend', this.setAfterTransitionFunc);
-      this.slidingArea.addEventListener('transitionend', this.setAfterTransitionFunc);
+      this.setAfterTransitionEventListener(false);
     }
   }
 
+  setTransitionPrefixed(value) {
+    this.slidingArea.style.transition = value;
+    this.slidingArea.style.webkitTransition = value;
+  }
+
+  setAfterTransitionEventListener(isRemove) {
+    const eventType = [DOMEventConstants.TRANSITION_END, DOMEventConstants.WEBKIT_TRANSITION_END];
+    eventType.map(type => this.slidingArea.removeEventListener(type, this._setAfterTransition));
+    if (!isRemove) {
+      eventType.map(type => this.slidingArea.addEventListener(type, this._setAfterTransition));
+    }
+  }
+
+  disableDrag() {
+    this.draggable = false;
+    setTimeout(() => (this.enableDrag()), this.props.duration);
+  }
+
+  enableDrag() {
+    this.draggable = true;
+    this._setAfterTransition();
+  }
+
   removeSliderTransition() {
+    if (
+      this.slidingArea === undefined
+      || this.slidingArea === null
+      || typeof this.slidingArea === 'string'
+      || this.slidingArea instanceof String
+    ) {
+      return;
+    }
     this.slidingArea.style.transition = `none`;
     this.slidingArea.removeEventListener('transitionend', this.setAfterTransitionFunc);
   }
@@ -267,6 +299,10 @@ class Carousel extends React.Component {
   nextSlide() {
     const { isInfinite } = this.props;
     const { currentSlide, slideCount } = this.state;
+
+    if (!this.draggable) {
+      return;
+    }
 
     if (!isInfinite && currentSlide === slideCount) {
       this.setState({
@@ -320,6 +356,18 @@ class Carousel extends React.Component {
     return 0;
   }
 
+  isMobile() {
+    return (
+      navigator.userAgent.match(/Android/i)
+      || navigator.userAgent.match(/webOS/i)
+      || navigator.userAgent.match(/iPhone/i)
+      || navigator.userAgent.match(/iPad/i)
+      || navigator.userAgent.match(/iPod/i)
+      || navigator.userAgent.match(/BlackBerry/i)
+      || navigator.userAgent.match(/Windows Phone/i)
+    );
+  }
+
   handleFocusOn() {
     if (this.props.isAutoPlay) {
       this.setTimerStop();
@@ -333,13 +381,12 @@ class Carousel extends React.Component {
   }
 
   handleSwipe() {
-    const { elementWidth } = this.props;
     this.clickSafe = (typeof (this.touchObject.length) !== 'undefined' && this.touchObject.length > 44);
     this.setSliderTransition();
     if (!this.clickSafe) {
       return;
     }
-    if (this.touchObject.length > (elementWidth / 2)) {
+    if (this.touchObject.length > (this.sliderItemWidth / 4)) {
       if (this.touchObject.direction === 1) {
         this.nextSlide();
       } else if (this.touchObject.direction === -1) {
@@ -349,24 +396,49 @@ class Carousel extends React.Component {
     this.touchObject = {};
   }
 
+  handleClick(e) {
+    if (this.clickSafe === true) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      if (e.nativeEvent) {
+        e.nativeEvent.stopPropagation();
+      }
+    }
+  }
 
   // event bind based Functions ------------------------------------------------------------
+  _setCurrentSlideAndPosition(props) {
+    let currentSlide = 1;
+    const state = {};
+    state.slideCount = props.children.length;
+    this.maximumDrag = (props.containerWidth / 5) * 4;
+    if (props.lastState) {
+      currentSlide = props.lastState.index;
+    } else if (props.targetSlide > 1) {
+      currentSlide = props.targetSlide;
+    }
+    const isInRange = (currentSlide >= 1) && (currentSlide <= state.slideCount);
+    state.currentSlide = isInRange ? currentSlide : 1;
+    state.currentPositionX = this.getHorizontalPosition(state.currentSlide);
+    this.setState(state);
+  }
+
   _setAfterTransition() {
     const { currentSlide, slideCount } = this.state;
     this.removeSliderTransition();
 
-    if (currentSlide === (slideCount + 1)) {
+    if (currentSlide > slideCount) {
       this.setState({
         currentSlide: 1,
         currentPositionX: this.getHorizontalPosition(1)
       });
-    } else if (currentSlide === 0) {
+    } else if (currentSlide < 1) {
       this.setState({
         currentSlide: slideCount,
         currentPositionX: this.getHorizontalPosition(slideCount)
       });
     }
-    this.setState({ dragging: false });
   }
 
   _setAfterWindowResize() {
@@ -396,66 +468,88 @@ class Carousel extends React.Component {
   }
   // event bind based Functions ------------------------------------------------------------
 
-
   renderSlider() {
     const {
       children,
       isInfinite,
-      elementWidth,
       isFullWidth,
     } = this.props;
     const { currentSlide } = this.state;
     const length = children.length;
-    const itemArray = [];
+    const sliders = [];
     let itemWidth = '';
 
     if (isFullWidth) {
       itemWidth = '100%';
     } else {
-      itemWidth = `${elementWidth}px`;
+      itemWidth = `${this.sliderItemWidth}px`;
+    }
+
+    if (!Array.isArray(children) || children.length === 1) {
+      return (
+        <li
+          className="slide_item active"
+          key="slide_1"
+          style={{ width: itemWidth }}
+        >
+          {children}
+        </li>
+      );
     }
 
     if (isInfinite) {
-      itemArray.push((
+      sliders.push((
         <li
           className="slide_item"
           key="slide_negative_2"
           style={{ width: itemWidth }}
-        >{children[length - 2]}</li>
+        >
+          {children[length - 2]}
+        </li>
       ));
-      itemArray.push((
+      sliders.push((
         <li
           className="slide_item"
           key="slide_negative_1"
           style={{ width: itemWidth }}
-        >{children[length - 1]}</li>
+        >
+          {children[length - 1]}
+        </li>
       ));
     }
 
-    children.map((child, idx) => itemArray.push((
+    children.map((child, idx) => sliders.push((
       <li
         className={`slide_item${(idx + 1) === currentSlide ? ' active' : ''}`}
         key={`slide_${idx + 1}`}
         style={{ width: itemWidth }}
-      >{child}</li>
+      >
+        {child}
+      </li>
     )));
 
     if (isInfinite) {
-      itemArray.push((
+      sliders.push((
         <li
           className="slide_item"
           key="slide_plus_1"
           style={{ width: itemWidth }}
-        >{children[0]}</li>));
-      itemArray.push((
+        >
+          {children[0]}
+        </li>
+      ));
+      sliders.push((
         <li
           className="slide_item"
           key="slide_plus_2"
           style={{ width: itemWidth }}
-        >{children[1]}</li>));
+        >
+          {children[1]}
+        </li>
+      ));
     }
 
-    return itemArray;
+    return sliders;
   }
 
   renderDotNavigator() {
@@ -505,35 +599,58 @@ class Carousel extends React.Component {
   }
 
   render() {
-    const { isMobile } = this.props;
+    const { children } = this.props;
     const { currentPositionX, tempPositionX } = this.state;
 
-    let sliderEvents = {
-      onTouchStart: e => this.onTouchStart(e),
-      onTouchMove: e => this.onTouchMove(e),
-      onTouchEnd: e => this.onTouchEnd(e),
-      onTouchCancel: e => this.onTouchCancel(e)
-    };
-    if (!isMobile) {
-      sliderEvents = {
-        onMouseOver: e => this.onMouseOver(e),
-        onMouseOut: e => this.onMouseOut(e),
-        onMouseDown: e => this.onMouseDown(e),
-        onMouseMove: e => this.onMouseMove(e),
-        onMouseUp: e => this.onMouseUp(e),
-        onMouseLeave: e => this.onMouseLeave(e)
-      };
+    if (children === undefined || children === null) {
+      return null;
     }
+
+    if (!Array.isArray(children) || children.length === 1) {
+      return (
+        <div className="carousel_slider_wrapper">
+          <ul
+            className="slider"
+            ref={ul => { this.slidingArea = ul; }}
+          >
+            {this.renderSlider()}
+          </ul>
+          {this.renderNavigator()}
+        </div>
+      );
+    }
+
+    const sliderEvents = {};
+    if (this.isMobile()) {
+      sliderEvents.onTouchStart = e => this.onTouchStart(e);
+      sliderEvents.onTouchMove = e => this.onTouchMove(e);
+      sliderEvents.onTouchEnd = e => this.onTouchEnd(e);
+      sliderEvents.onTouchCancel = e => this.onTouchCancel(e);
+    } else {
+      sliderEvents.onMouseOver = e => this.onMouseOver(e);
+      sliderEvents.onMouseOut = e => this.onMouseOut(e);
+      sliderEvents.onMouseDown = e => this.onMouseDown(e);
+      sliderEvents.onMouseMove = e => this.onMouseMove(e);
+      sliderEvents.onMouseUp = e => this.onMouseUp(e);
+      sliderEvents.onMouseLeave = e => this.onMouseLeave(e);
+    }
+
     return (
       <div
         className="carousel_slider_wrapper"
       >
         <ul
           className="slider"
+          role="presentation"
           ref={ul => { this.slidingArea = ul; }}
           {...sliderEvents}
+          onClick={e => this.handleClick(e)}
           style={{
-            transform: `translate3d(${tempPositionX !== undefined ? tempPositionX : currentPositionX}px, 0, 0)`,
+            transform: `translateX(${tempPositionX !== undefined ? tempPositionX : currentPositionX}px)`,
+            WebkitTransform: `translateX(${tempPositionX !== undefined ? tempPositionX : currentPositionX}px)`,
+            MozTransform: `translateX(${tempPositionX !== undefined ? tempPositionX : currentPositionX}px)`,
+            OTransform: `translateX(${tempPositionX !== undefined ? tempPositionX : currentPositionX}px)`,
+            msTransform: `translateX(${tempPositionX !== undefined ? tempPositionX : currentPositionX}px)`,
           }}
         >
           {this.renderSlider()}
@@ -546,7 +663,6 @@ class Carousel extends React.Component {
 
 Carousel.propTypes = {
   children: PropTypes.node,
-  isMobile: PropTypes.bool,
   naviType: PropTypes.string,
   isFullWidth: PropTypes.bool,
   containerWidth: PropTypes.number,
@@ -557,10 +673,14 @@ Carousel.propTypes = {
   isInfinite: PropTypes.bool,
   targetSlide: PropTypes.number,
   cssEase: PropTypes.string,
+  isResetToFirst: PropTypes.bool,
+  lastState: PropTypes.object,
+  saveStateFunc: PropTypes.func
 };
 
 Carousel.defaultProps = {
-  isMobile: Boolean(true),
+  children: null,
+  saveStateFunc: null,
   naviType: CarouselNavigatorType.DOT,
   isFullWidth: Boolean(true),
   containerWidth: window.innerWidth,
@@ -569,7 +689,9 @@ Carousel.defaultProps = {
   autoPlayInterval: 1000,
   isInfinite: Boolean(true),
   targetSlide: 1,
-  cssEase: 'cubic-bezier(0.420, 0.000, 0.580, 1.000)',
+  isResetToFirst: Boolean(false),
+  cssEase: 'ease-out',
+  lastState: undefined
 };
 
 export default Carousel;
